7cb004ed323273894eb650c7a508f2097b9e1d57
cammy <alba99a@gmail.com>
2020-12-03 17:23:58 +0000

    Update config.cpp

--- @3PARA-GU Mods/Addons/3para_unit/config.cpp
+++ @3PARA-GU Mods/Addons/3para_unit/config.cpp
@@ -2228,70 +2228,104 @@ class cfgVehicles
 		};
 	};
 
+	class UK3CB_BAF_Landrover_Amb_FFR_Green_A;
+	class UK3CB_BAF_Landrover_Amb_FFR_Sand_A;
+	class UK3CB_BAF_Landrover_Hard_FFR_Green_A;
+	class UK3CB_BAF_Landrover_Hard_FFR_Green_B;
+	class UK3CB_BAF_Landrover_Hard_FFR_Arctic_A;
+	class UK3CB_BAF_Landrover_Hard_FFR_Sand_A;
+	class UK3CB_BAF_Landrover_Hard_FFR_MERT_A;
+	class UK3CB_BAF_Landrover_Snatch_FFR_Green_A;
+	class UK3CB_BAF_Landrover_Snatch_FFR_Sand_A;
+	class UK3CB_BAF_Landrover_Soft_FFR_Green_A;
+	class UK3CB_BAF_Landrover_Soft_FFR_Green_B;
+	class UK3CB_BAF_Landrover_Soft_FFR_Arctic_A;
+	class UK3CB_BAF_Landrover_Soft_FFR_Sand_A;
+	class UK3CB_BAF_Landrover_Soft_FFR_MERT_A;
+	class UK3CB_BAF_Landrover_WMIK_GMG_FFR_Green_A;
+	class UK3CB_BAF_Landrover_WMIK_GMG_FFR_Green_B;
+	class UK3CB_BAF_Landrover_WMIK_GMG_FFR_Sand_A;
+	class UK3CB_BAF_Landrover_WMIK_GPMG_FFR_Green_A;
+	class UK3CB_BAF_Landrover_WMIK_GPMG_FFR_Green_B;
+	class UK3CB_BAF_Landrover_WMIK_GPMG_FFR_Sand_A;
+	class UK3CB_BAF_Landrover_WMIK_HMG_FFR_Green_A;
+	class UK3CB_BAF_Landrover_WMIK_HMG_FFR_Green_B;
+	class UK3CB_BAF_Landrover_WMIK_HMG_FFR_Sand_A;
+	class UK3CB_BAF_Landrover_WMIK_Milan_FFR_Green_A;
+	class UK3CB_BAF_Landrover_WMIK_Milan_FFR_Green_B;
+	class UK3CB_BAF_Landrover_WMIK_Milan_FFR_Sand_A;
+	class UK3CB_BAF_Husky_Logistics_GMG_Sand;
+	class UK3CB_BAF_Husky_Logistics_GMG_Green;
+	class UK3CB_BAF_Husky_Logistics_HMG_Sand;
+	class UK3CB_BAF_Husky_Logistics_HMG_Green;
+	class UK3CB_BAF_Husky_Logistics_GPMG_Sand;
+	class UK3CB_BAF_Husky_Logistics_GPMG_Green;
+	class UK3CB_BAF_Husky_Passenger_GMG_Green;
+	class UK3CB_BAF_Husky_Passenger_HMG_Sand;
+	class UK3CB_BAF_Husky_Passenger_HMG_Green;
+	class UK3CB_BAF_Husky_Passenger_GPMG_Sand;
+	class UK3CB_BAF_Husky_Passenger_GPMG_Green;
+	class UK3CB_BAF_Husky_Passenger_GMG_Sand;
+	class UK3CB_BAF_Jackal2_L134A1_D;
+	class UK3CB_BAF_Jackal2_L134A1_W;
+	class UK3CB_BAF_Jackal2_L134A1_D2;
+	class UK3CB_BAF_Jackal2_L134A1_G;
+	class UK3CB_BAF_Jackal2_L111A1_D;
+	class UK3CB_BAF_Jackal2_L111A1_W;
+	class UK3CB_BAF_Jackal2_L111A1_D2;
+	class UK3CB_BAF_Jackal2_L111A1_G;
+	class UK3CB_BAF_Coyote_Logistics_L134A1_D;
+	class UK3CB_BAF_Coyote_Logistics_L134A1_W;
+	class UK3CB_BAF_Coyote_Logistics_L134A1_D2;
+	class UK3CB_BAF_Coyote_Logistics_L134A1_G;
+	class UK3CB_BAF_Coyote_Logistics_L111A1_D;
+	class UK3CB_BAF_Coyote_Logistics_L111A1_W;
+	class UK3CB_BAF_Coyote_Logistics_L111A1_D2;
+	class UK3CB_BAF_Coyote_Logistics_L111A1_G;
+	class UK3CB_BAF_Coyote_Passenger_L134A1_D;
+	class UK3CB_BAF_Coyote_Passenger_L134A1_W;
+	class UK3CB_BAF_Coyote_Passenger_L134A1_D2;
+	class UK3CB_BAF_Coyote_Passenger_L134A1_G;
+	class UK3CB_BAF_Coyote_Passenger_L111A1_D;
+	class UK3CB_BAF_Coyote_Passenger_L111A1_W;
+	class UK3CB_BAF_Coyote_Passenger_L111A1_D2;
+	class UK3CB_BAF_Coyote_Passenger_L111A1_G;
+	class rksla3_aw159_hma2;
+	class UK3CB_BAF_Apache_AH1_DynamicLoadoutUnlimited;
+	class Boeing_CH_47_F_R;
+	class B_Heli_Light_01_dynamicLoadout_F;
+	class B_Heli_Light_01_F;
+	class UK3CB_BAF_FV432_Mk3_RWS_Green;
+	class UK3CB_BAF_FV432_Mk3_RWS_Sand;
+	class UK3CB_BAF_FV432_Mk3_GPMG_Green;
+	class UK3CB_BAF_FV432_Mk3_GPMG_Sand;
+	class UK3CB_BAF_Warrior_A3_D_Camo;
+	class UK3CB_BAF_Warrior_A3_W_Camo;
+	class UK3CB_BAF_Warrior_A3_D_Cage_Camo;
+	class UK3CB_BAF_Warrior_A3_W_Cage_Camo;
+	class UK3CB_BAF_Warrior_A3_D_Cage;
+	class UK3CB_BAF_Warrior_A3_W_Cage;
+	class UK3CB_BAF_Warrior_A3_D;
+	class UK3CB_BAF_Warrior_A3_W;
+	class UK3CB_BAF_RHIB_GPMG;
+	class UK3CB_BAF_RHIB_HMG;
+	class UK3CB_BAF_Panther_GPMG_Sand_A;
+	class UK3CB_BAF_Panther_GPMG_Green_A;
 	
-	/*start of the vehicles with custom loadouts 
-	need all class names of all vehicles 
-	3CB Landrovers
-	
-	3CB Husky ✓
-	"UK3CB_BAF_Husky_Logistics_GMG_Sand" "UK3CB_BAF_Husky_Logistics_GMG_Green" "UK3CB_BAF_Husky_Logistics_HMG_Sand") "UK3CB_BAF_Husky_Logistics_HMG_Green" "UK3CB_BAF_Husky_Logistics_GPMG_Sand" "UK3CB_BAF_Husky_Logistics_GPMG_Green" 
-	"UK3CB_BAF_Husky_Passenger_GMG_Green" "UK3CB_BAF_Husky_Passenger_HMG_Sand") "UK3CB_BAF_Husky_Passenger_HMG_Green" "UK3CB_BAF_Husky_Passenger_GPMG_Sand" "UK3CB_BAF_Husky_Passenger_GPMG_Green" "UK3CB_BAF_Husky_Passenger_GMG_Sand"
-	3CB Jackal ✓
-	"UK3CB_BAF_Jackal2_L134A1_D" "UK3CB_BAF_Jackal2_L134A1_W" "UK3CB_BAF_Jackal2_L134A1_D2" "UK3CB_BAF_Jackal2_L134A1_G" 
-	"UK3CB_BAF_Jackal2_L111A1_D" "UK3CB_BAF_Jackal2_L111A1_W" "UK3CB_BAF_Jackal2_L111A1_D2" "UK3CB_BAF_Jackal2_L111A1_G"	
-	3CB Coyote ✓
-	"UK3CB_BAF_Coyote_Logistics_L134A1_D" "UK3CB_BAF_Coyote_Logistics_L134A1_W" "UK3CB_BAF_Coyote_Logistics_L134A1_D2" "UK3CB_BAF_Coyote_Logistics_L134A1_G" 
-	"UK3CB_BAF_Coyote_Logistics_L111A1_D" "UK3CB_BAF_Coyote_Logistics_L111A1_W" "UK3CB_BAF_Coyote_Logistics_L111A1_D2" "UK3CB_BAF_Coyote_Logistics_L111A1_G"
-	"UK3CB_BAF_Coyote_Passenger_L134A1_D" "UK3CB_BAF_Coyote_Passenger_L134A1_W" "UK3CB_BAF_Coyote_Passenger_L134A1_D2" "UK3CB_BAF_Coyote_Passenger_L134A1_G"
-	"UK3CB_BAF_Coyote_Passenger_L111A1_D" "UK3CB_BAF_Coyote_Passenger_L111A1_W" "UK3CB_BAF_Coyote_Passenger_L111A1_D2" "UK3CB_BAF_Coyote_Passenger_L111A1_G"
-	3CB Man trucks 
-	
-	RKSL Wildcat✓
-	"rksla3_aw159_hma2"
-	3CB Apache Generic ✓
-	"UK3CB_BAF_Apache_AH1_DynamicLoadoutUnlimited"
-	boeing chinook (RAF) ✓
-	"Boeing_CH_47_F_R"
-	little birds
-	
-	3CB bulldog ✓
-	"UK3CB_BAF_FV432_Mk3_RWS_Green" "UK3CB_BAF_FV432_Mk3_RWS_Sand" "UK3CB_BAF_FV432_Mk3_GPMG_Green" "UK3CB_BAF_FV432_Mk3_GPMG_Sand"
-	3CB warrior ✓
-	"UK3CB_BAF_Warrior_A3_D_Camo" "UK3CB_BAF_Warrior_A3_W_Camo" "UK3CB_BAF_Warrior_A3_D_Cage_Camo" "UK3CB_BAF_Warrior_A3_W_Cage_Camo" "UK3CB_BAF_Warrior_A3_D_Cage" "UK3CB_BAF_Warrior_A3_W_Cage" "UK3CB_BAF_Warrior_A3_D" "UK3CB_BAF_Warrior_A3_W"
-	3CB offshore raiding craft ✓
-	"UK3CB_BAF_RHIB_GPMG" "UK3CB_BAF_RHIB_HMG"
-	3CB Panther ✓
-	"UK3CB_BAF_Panther_GPMG_Sand_A" "UK3CB_BAF_Panther_GPMG_Green_A" 
-	*/
-	/*class B_Heli_light_01_F;
     class My_New_Heli: B_Heli_Light_01_F 
     {
         faction = "B_3para"; 
         side = 1;
-        crew = "B_Helipilot_F"; //or if you make your own crew... crew = "TEST_Heli_pilot";
-        class TransportItems //Cargo Items
+        class TransportItems
         {
             class _xx_Medikit
             {
                 name = "Medikit";
                 count = 1;
             };
-            class _xx_Toolkit
-            {
-                name = "Toolkit";
-                count = 1;
-            };
-            class _xx_FirstAidKit
-            {
-                name = "FirstAidKit";
-                count = 5;
-            };
-            class _xx_30Rnd_65x39_caseless_mag
-            {
-                count = 25;
-            };
+
         };
-    };*/
+    };
 	
 	
 	
